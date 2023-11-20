2e03ebc1945fb831e061cd327e2a21fd30d82af2
Miguel Olvera <78117887+MiguelSix@users.noreply.github.com>
2023-11-20 00:52:31 -0600

    Delete SaleServiceRest directory

--- SaleServiceRest/src/main/java/com/itq/SaleService/service/CustomSaleException.java
+++
@@ -1,9 +0,0 @@
-package com.itq.saleService.service;
-
-@SuppressWarnings("serial")
-public class CustomSaleException extends RuntimeException {
-
-    public CustomSaleException(String message) {
-        super(message);
-    }
-}
